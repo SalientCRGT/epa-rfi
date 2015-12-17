dc21823a396c78aa0296b480f96e0406c526e249
selectshake <selectshake@gmail.com>
2015-12-17 21:40:47 +0000

    updated dbService to account for new database information

--- api/dbService.php
+++ api/dbService.php
@@ -6,9 +6,9 @@ class DbService {
 
     // MySQL Connection constants
     const DB_HOST = "127.0.0.1";
-    const DB_NAME = "4840w";
-    const DB_USERNAME = "4840w";
-    const DB_PASSWORD = "4840w";
+    const DB_NAME = "4840epa";
+    const DB_USERNAME = "4840epa";
+    const DB_PASSWORD = "4840epa";
 
     // Status Codes
     const ERROR_CODE = "error";
@@ -21,7 +21,7 @@ class DbService {
 
     function __construct() {
         // Open database connection
-        $this->dbConnection = new PDO("mysql:host=" .static::DB_HOST. ";dbname=" .static::DB_NAME, static::DB_USERNAME, static::DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $this->dbConnection = new PDO("mysql:host=" .static::DB_HOST. ";dbname=" .static::DB_NAME, static::DB_USERNAME, static::DB_PASSWORD);
         $this->dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $this->sessionId = session_id();
@@ -36,7 +36,7 @@ class DbService {
         
         try {
             // Retrieve Open FDA api key
-            $sql = "SELECT * FROM FRS_STATE_FACILITY where POSTAL_CODE like '22046%'";
+            $sql = "SELECT * FROM frs_state_facility where postal_code like '22046%'";
             $stmt = $this->dbConnection->prepare($sql);
             $stmt->execute();
             $resultsArr = $stmt->fetchAll(PDO::FETCH_CLASS);
@@ -61,7 +61,7 @@ class DbService {
     function getFacilityById($registryId){
         
         try {
-            $sql = "SELECT * FROM FRS_STATE_FACILITY where REGISTRY_ID = :registryId";
+            $sql = "SELECT * FROM frs_state_facility where registry_id = :registryId";
             $stmt = $this->dbConnection->prepare($sql);
             $stmt->bindValue(':registryId', $registryId);
             $stmt->execute();
