63c0344e042ea1db7098174c442c688863fad4cc
Frank Zubko <frank.zubko@gmail.com>
2015-12-17 22:06:17 +0000

    basic angular resource setup

--- ngapp/ngapp.js
+++ ngapp/ngapp.js
@@ -1,6 +1,6 @@
-var app = angular.module('app', ['ngRoute']);//,'oc.lazyLoad']);
+var app = angular.module('app', ['ngRoute','ngResource']);//,'oc.lazyLoad']);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, $resourceProvider){
 
 	$routeProvider
 		.when('/', {
@@ -10,14 +10,44 @@ app.config(function($routeProvider){
 		.otherwise({
 			redirectTo: '/'
 		});
+	
+	// reduces response from {code:"", msg:"", data:{}} to just data
+	function transformOne(data){
+		var jsonData = angular.fromJson(data);
+		var result = jsonData.data || {};
+		result.$code = jsonData.code;
+		result.$msg = jsonData.msg;
+		return result;
+	}
+
+	// reduces response from {code:"", msg:"", data:[]} to just data
+	function transformAll(data){
+		var jsonData = angular.fromJson(data);
+		var result = jsonData.data || [];
+		result.$code = jsonData.code;
+		result.$msg = jsonData.msg;
+		return result;
+	}
+
+	$resourceProvider.defaults.actions = {
+		get: {
+			method: 'GET',
+			params: {id:'@registryId'},
+			transformResponse: transformOne
+		},
+		query: {
+			method: 'GET',
+			isArray: true,
+			transformResponse: transformAll
+		}
+	};
 
 });
 
-// force the reoute event
-//app.run(['$route', function($route)  {
-//	$route.reload();
-//}]);
+app.factory('FacilityResource',function($resource){
+	return $resource('/api/facilities/:id');
+});
 
-app.controller('SplashCtrl', function(){
-    console.log('splashCtrl');
+app.controller('SplashCtrl', function($scope, FacilityResource){
+    $scope.facilities = FacilityResource.query({});
 });
