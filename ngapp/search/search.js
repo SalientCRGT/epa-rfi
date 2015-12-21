194c17da613fbef3a8ba4f4fb04c4cc69bfe3d26
Frank Zubko <frank.zubko@gmail.com>
2015-12-21 21:08:45 +0000

    Added searching by field

--- ngapp/search/search.js
+++ ngapp/search/search.js
@@ -1,10 +1,14 @@
-ngapp.controller('SearchCtrl', function($scope, $http, facilities){
+ngapp.controller('SearchCtrl', function($scope, $http, facilities, FacilityResource){
     $scope.facilities = facilities;
+    
+    $scope.search = function(){
+        $scope.facilities = FacilityResource.query($scope.searchParameters);
+    };
 
     $scope.getFacilities = function(){
-    	console.log($scope.search);
-    	if($scope.search.length > 3){
-	    	$scope.facilities = $http.get('/api/facilities/search/' + $scope.search.split(' ').join('+'))
+    	console.log($scope.q);
+    	if($scope.q.length > 3){
+	    	$scope.facilities = $http.get('/api/facilities/search/' + $scope.q.split(' ').join('+'))
 	    		.then(function(response) {
 	    			$scope.facilities = $scope.transformAll(response.data);
 	    			console.log('facilities: ' + $scope.facilities);
