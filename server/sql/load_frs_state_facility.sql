a8f4f1da79539faf96b32dd6bcbb1af2f04bc5fb
selectshake <selectshake@gmail.com>
2015-12-23 14:44:53 +0000

    Trying some more

--- server/sql/load_frs_state_facility.sql
+++ server/sql/load_frs_state_facility.sql
@@ -1,4 +1,4 @@
 
-LOAD DATA INFILE '/var/lib/jenkins/workspace/4840W/epa-agile-rfi/epa_db_runscript/server/data/VA_FACILITY_FILE.csv' INTO TABLE frs_state_facility
+LOAD DATA LOCAL INFILE '/var/lib/jenkins/workspace/4840W/epa-agile-rfi/epa_db_runscript/server/data/VA_FACILITY_FILE.csv' INTO TABLE frs_state_facility
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n' STARTING BY '';
